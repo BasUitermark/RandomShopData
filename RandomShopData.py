75784ddeb70084c4d1e119a82b9eb1ce0071a35c
BasUitermark <basuitermark@outlook.com>
2023-04-10 22:52:04 +0200

    added input

--- RandomShopData.py
+++ RandomShopData.py
@@ -5,5 +5,8 @@ def read(path):
 	return df
 
 if __name__ == "__main__":
+	RegionWealth = int(input("Specify city wealth (1 - 10): "))
+	if RegionWealth not in range(1, 10):
+		exit(1)
+	print("City wealth is: ", RegionWealth)
 	blacksmith_RawData = read('data/DnD Shop Price Data - Blacksmith.csv')
-	print(blacksmith_RawData)
