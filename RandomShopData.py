f77cde533ac548c716b51907588f2e89586c7dbc
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-04-12 14:03:21 +0200

    Added test .csv

--- RandomShopData.py
+++ RandomShopData.py
@@ -1,14 +1,23 @@
 import pandas as pd
 from termcolor import colored
 from simple_term_menu import TerminalMenu
+import glob
 
 def read(path):
 	df = pd.read_csv(path)
 	return df
 
 def RandomShopData():
+	entries = []
+	for file in glob.glob(r"data/*.csv"):
+		entries.append(file)
 	print("Select one of the following data entries")
-	#read through the data folder for sheets and display the names in term_menu
+	terminal_menu = TerminalMenu(entries)
+	entry_index = terminal_menu.show()
+
+	df = read(entries[entry_index])
+	print (df)
+	
 
 def MakeNewShop():
 	userInput = input("Please specify shop name: ")
@@ -19,10 +28,10 @@ if __name__ == "__main__":
 	print(colored("Welcome to RandomShopData for TRGP!\n", "green"), colored("What would you like to do?", "green"))
 	options = ["Randomize prices for existing shop data", "Make new shop", "Add item to shop", "Change region wealth statistic"]
 	terminal_menu = TerminalMenu(options)
-	menu_entry_index = terminal_menu.show()
-	if menu_entry_index == 0:
+	entry_index = terminal_menu.show()
+	if entry_index == 0:
 		RandomShopData()
-	if menu_entry_index == 1:
+	if entry_index == 1:
 		MakeNewShop()
 	# if userInput is 1:
 	# 	RandomShopData()
@@ -32,4 +41,3 @@ if __name__ == "__main__":
 	# if RegionWealth not in range(1, 10):
 	# 	exit(1)
 	# print("City wealth is: ", RegionWealth)
-	# blacksmith_RawData = read('data/DnD Shop Price Data - Blacksmith.csv')
