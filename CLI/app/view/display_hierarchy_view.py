ac4802bcb1551196ee49380e05acd9caff84e266
Bas Uitermark <88148607+BasUitermark@users.noreply.github.com>
2023-05-26 15:05:28 +0200

    Added color to show data

--- CLI/app/view/display_hierarchy_view.py
+++ CLI/app/view/display_hierarchy_view.py
@@ -1,6 +1,7 @@
 from controller.handle_shop import ShopHandler
 from controller.handle_city import CityHandler
 from controller.handle_kingdom import KingdomHandler
+from termcolor import colored
 
 def display_hierarchical_view(session):
     # Create handlers
@@ -11,14 +12,14 @@ def display_hierarchical_view(session):
     # Get all kingdoms
     kingdoms = kingdom_handler.select_all(session)
     for kingdom in kingdoms:
-        print(kingdom.name)
+        print(colored(">", attrs=['bold']),colored(kingdom.name, 'green', attrs=['bold']))
         
         # Get all cities in the current kingdom
         cities = city_handler.select_by_kingdom(session, kingdom.id)
         for city in cities:
-            print("\t", city.name)
+            print(colored(">\t", attrs=['bold']), colored(city.name, 'green', attrs=['bold']))
             
             # Get all shops in the current city
             shops = shop_handler.select_by_city(session, city.id)
             for shop in shops:
-                print("\t\t", shop.name)
+                print(colored(">\t\t", attrs=['bold']), colored(shop.name, 'green', attrs=['bold']))
